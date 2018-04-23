b0ad579a1332e77d0383f4aadcab35e6712340b2
Wyn Van Devanter <wyn.vandevanter@excella.com>
2018-04-23 18:07:43 -0400

    pm guard

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -130,34 +130,16 @@ object Main extends App {
   }
 
   // Pattern guards - if statements after case expression
-  def showImportantNotification(notification: Notification, importantPeopleInfo: Seq[String]): String = {
-    notification match {
-      case Email(email, _, _) if importantPeopleInfo.contains(email) =>
-        "You got an email from special someone!"
-      case SMS(number, _) if importantPeopleInfo.contains(number) =>
-        "You got an SMS from special someone!"
-      case other =>
-        showNotification(other) // nothing special, delegate to our original showNotification function
-    }
+  val selection2 = "Twenty-five"
+  selection2 match {
+    case "Twenty" => println("Twenty!")
+    case "Twenty-five" if (selection == "One") => println("Big jump up from your last selection!")
   }
-
-  val importantPeopleInfo = Seq("867-5309", "jenny@gmail.com")
-
-  val someSms = SMS("867-5309", "Are you there?")
-  val someVoiceRecording = VoiceRecording("Tom", "voicerecording.org/id/123")
-  val importantEmail = Email("jenny@gmail.com", "Drinks tonight?", "I'm free after 5!")
-  val importantSms = SMS("867-5309", "I'm here! Where are you?")
-
-  println(showImportantNotification(someSms, importantPeopleInfo))
-  println(showImportantNotification(someVoiceRecording, importantPeopleInfo))
-  println(showImportantNotification(importantEmail, importantPeopleInfo))
-  println(showImportantNotification(importantSms, importantPeopleInfo))
 
   /// 2.6 Type Classes
 
   /// 2.7 Concurrency
 
   /// 2.8 Testing
-
 
 }
