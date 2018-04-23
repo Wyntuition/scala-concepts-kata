ed6cc58269f29440efff09ecc2cd4debaeaa3598
Wyn Van Devanter <wyn.vandevanter@excella.com>
2018-04-23 17:54:46 -0400

    start sbt on compose up

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -122,12 +122,36 @@ object Main extends App {
   /// 2.5 Pattern Matching
   println("-----Pattern mstching-----")
 
-  val selection = "One"  
+  val selection = "One"
   selection match {  
     case "One" => println("You selected option one!")  
     case "Two" => println("You selected option two!")  
     case _ => println("You selected something else: ")  
   }
+
+  // Pattern guards - if statements after case expression
+  def showImportantNotification(notification: Notification, importantPeopleInfo: Seq[String]): String = {
+    notification match {
+      case Email(email, _, _) if importantPeopleInfo.contains(email) =>
+        "You got an email from special someone!"
+      case SMS(number, _) if importantPeopleInfo.contains(number) =>
+        "You got an SMS from special someone!"
+      case other =>
+        showNotification(other) // nothing special, delegate to our original showNotification function
+    }
+  }
+
+  val importantPeopleInfo = Seq("867-5309", "jenny@gmail.com")
+
+  val someSms = SMS("867-5309", "Are you there?")
+  val someVoiceRecording = VoiceRecording("Tom", "voicerecording.org/id/123")
+  val importantEmail = Email("jenny@gmail.com", "Drinks tonight?", "I'm free after 5!")
+  val importantSms = SMS("867-5309", "I'm here! Where are you?")
+
+  println(showImportantNotification(someSms, importantPeopleInfo))
+  println(showImportantNotification(someVoiceRecording, importantPeopleInfo))
+  println(showImportantNotification(importantEmail, importantPeopleInfo))
+  println(showImportantNotification(importantSms, importantPeopleInfo))
 
   /// 2.6 Type Classes
 
